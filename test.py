9e821372af6fc9f8d1d76df7a5bf139ffcfdd61d
liangye <liangye1230@126.com>
2016-07-27 14:10:08 -0700

    master clean.

--- test.py
+++ test.py
@@ -25,19 +25,12 @@ print(m.mo_energy)
 ne = mol.nelectron/2 #electron per string
 no = len(m.mo_energy)
 ns = math_C(no,ne)
-#print 'ns = ',ns
 Z = constructZ(ne,no)
 #print 'Z'
 #print Z
-print 'ne =', ne, 'no =', no
-mocc = m.mo_coeff[:,m.mo_occ>0]
-mvir = m.mo_coeff[:,m.mo_occ==0]
-print 'mocc'
-print mocc
-print 'mvir'
-print mvir
+print 'ne =', ne, 'no =', no, 'ns =', ns
 
-#print '################## h_mtx'
+###########################################################
 h_ao_mtx = scf.hf.get_hcore(mol)
 h_mtx = numpy.dot(numpy.dot(m.mo_coeff.T,h_ao_mtx),m.mo_coeff)
 print 'h matrix'
@@ -47,40 +40,16 @@ print h_mtx
 #print 'eri'
 #print(eri.shape)
 #print eri
-g_mtx = numpy.zeros([no,]*4)
+g_mtx = ao2mo.kernel(mol, m.mo_coeff, compact=False)
 k_mtx = numpy.zeros([no,no])
-for i in range(no):
-    for j in range(no):
-        for r in range(no):
-            for s in range(no):
-                g_mtx[i,r,s,j] = ao2mo.kernel(mol, (m.mo_coeff[:,i:i+1],m.mo_coeff[:,r:r+1],m.mo_coeff[:,s:s+1],m.mo_coeff[:,j:j+1]), compact=False)
-
-            k_mtx[i,j] -= g_mtx[i,r,r,j]
+for i in xrange(no):
+    for j in xrange(no):
+        for r in xrange(no):
+            k_mtx[i,j] -= g_mtx[i*no+r,r*no+j]
         k_mtx[i,j] *= 0.5
 k_mtx = k_mtx + h_mtx
 print 'g matrix'
-print g_mtx[0,0,0,0]
-#print '################## k_mtx'
-#print k_mtx
-
-e_mtx = numpy.zeros([ns,ns])
-for i in range(ns):
-    for j in range(ns):
-        #energy of config i,j
-        orderi,orderj = getOrder(ne,no,i,Z),getOrder(ne,no,j,Z)
-        for p in range(no):
-            if orderi[p] == 1: e_mtx[i,j] += h_mtx[p,p]
-            if orderj[p] == 1: e_mtx[i,j] += h_mtx[p,p]
-        for p in range(no):
-            for q in range(p+1,no):
-                if orderi[p] and orderi[q]: e_mtx[i,j] += (g_mtx[p,p,q,q] - g_mtx[p,q,q,p])
-                if orderj[p] and orderj[q]: e_mtx[i,j] += (g_mtx[p,p,q,q] - g_mtx[p,q,q,p])
-        for p in range(no):
-            for q in range(no):
-                if orderi[p] and orderj[q]: e_mtx[i,j] += g_mtx[p,p,q,q]
-
-print 'energy mtx for states'
-print e_mtx
+print g_mtx[0,0]
 ################################################################
 crt = 1e-9
 
@@ -94,50 +63,46 @@ print C0
 
 E0 = 0 
 #H0 = numpy.zeros([ns,ns,ns,ns]) #H0[kakb,jajb]
-#for i in range(ns):
-#    for j in range(ns):
+#for i in xrange(ns):
+#    for j in xrange(ns):
 #        H0[i,j,i,j] = 1
 ######################################################################
 while 1:
     D = numpy.zeros([no,no,ns,ns])
-    for r in range(no):
-        for s in range(no):
-            for ka in range(ns):
-                for kb in range(ns):
-                    for jb in range(ns):
-#                        print opr_E(ne,no,r,s,kb,jb,Z) 
-                        D[r,s,ka,kb] += opr_E(ne,no,r,s,kb,jb,Z) * C0[ka,jb] 
-                    for ja in range(ns):
-                        D[r,s,ka,kb] += opr_E(ne,no,r,s,ka,ja,Z) * C0[ja,kb]
+    for r in xrange(no):
+        for s in xrange(no):
+            for k in xrange(ns):
+                for j in xrange(ns):
+#                    print opr_E(ne,no,r,s,kb,jb,Z) 
+                    D[r,s,:,k] += opr_E(ne,no,r,s,k,j,Z) * C0[:,j] 
+                    D[r,s,k,:] += opr_E(ne,no,r,s,k,j,Z) * C0[j,:]
 #            print 'D(',r,',',s,') matrix'
 #            print D[r,s,:,:]
 
     G = numpy.zeros([no,no,ns,ns])
-    for p in range(no):
-        for q in range(no):
-            for ka in range(ns):
-                for kb in range(ns):
-                    for r in range(no):
-                        for s in range(no):
-                            G[p,q,ka,kb] += 0.5 * g_mtx[p,q,r,s] * D[r,s,ka,kb]
+    for p in xrange(no):
+        for q in xrange(no):
+            for r in xrange(no):
+                for s in xrange(no):
+                    G[p,q,:,:] += 0.5 * g_mtx[p*no+q,r*no+s] * D[r,s,:,:]
 #            print 'G(',p,',',q,') matrix'
 #            print G[p,q,:,:]
 
     sig2 = numpy.zeros([ns,ns])
-    for ia in range(ns):
-        for ib in range(ns):
-            for p in range(no):
-                for q in range(no):
-                    for ka in range(ns):
+    for ia in xrange(ns):
+        for ib in xrange(ns):
+            for p in xrange(no):
+                for q in xrange(no):
+                    for ka in xrange(ns):
                         sig2[ia,ib] += opr_E(ne,no,p,q,ia,ka,Z)*G[p,q,ka,ib]
-                    for kb in range(ns):
+                    for kb in xrange(ns):
                         sig2[ia,ib] += opr_E(ne,no,p,q,ib,kb,Z)*G[p,q,ia,kb]
 
     sig1 = numpy.zeros([ns,ns])
-    for ia in range(ns):
-        for ib in range(ns):
-            for p in range(no):
-                for q in range(no):
+    for ia in xrange(ns):
+        for ib in xrange(ns):
+            for p in xrange(no):
+                for q in xrange(no):
                     sig1[ia,ib] += k_mtx[p,q]*D[p,q,ia,ib]
 
     sig = numpy.zeros([ns,ns])
